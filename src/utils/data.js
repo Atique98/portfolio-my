5156f3650431a1d7dc6a0b57762e18c410373763
Atique <muhammadatique.bscs@gmail.com>
2024-01-03 17:58:22 +0500

    fix css and add work-exp

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,41 +1,71 @@
-export const SKILLS =[
-    {
-        title:"Frontend",
-        icon:"./assets/images/fronty.png",
-        skills:[
-            {skill: "HTML5", percentage:"80%"},
-            {skill: "CSS3(Sass/SCSS)", percentage:"80%"},
-            {skill: "JavaScript", percentage:"75%"},
-            {skill: "React.js", percentage:"90%"},
-        ],
-    },
-    {
-        title:"Backend",
-        icon:"./assets/images/backendy.png",
-        skills:[
-            {skill: "Node.js", percentage:"50%"},
-            {skill: "Java", percentage:"70%"},
-            {skill: "C# (DotNet)", percentage:"75%"},
-        ],
-    },
-    {
-        title:"Tools",
-        icon:"./assets/images/tooly.png",
-        skills:[
-            {skill: "Git & GitHub", percentage:"85%"},
-            {skill: "Visual Studio Code", percentage:"80%"},
-            {skill: "Docker", percentage:"50%"},
-            {skill: "Kubernetes", percentage:"65%"},
-            {skill: "Google Cloud", percentage:"65%"},
-        ],
-    },
-    {
-        title:"Soft Skills",
-        icon:"./assets/images/skilly.png",
-        skills:[
-            {skill: "Problem Solving", percentage:"70%"},
-            {skill: "Collaboration", percentage:"80%"},
-            {skill: "Attention to details", percentage:"85%"},
-        ],
-    },
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "./assets/images/fronty.png",
+    skills: [
+      { skill: "HTML5", percentage: "80%" },
+      { skill: "CSS3(Sass/SCSS)", percentage: "80%" },
+      { skill: "JavaScript", percentage: "75%" },
+      { skill: "React.js", percentage: "90%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./assets/images/backendy.png",
+    skills: [
+      { skill: "Node.js", percentage: "50%" },
+      { skill: "Java", percentage: "70%" },
+      { skill: "C# (DotNet)", percentage: "75%" },
+    ],
+  },
+  {
+    title: "Tools",
+    icon: "./assets/images/tooly.png",
+    skills: [
+      { skill: "Git & GitHub", percentage: "85%" },
+      { skill: "Visual Studio Code", percentage: "80%" },
+      { skill: "Docker", percentage: "50%" },
+      { skill: "Kubernetes", percentage: "65%" },
+      { skill: "Google Cloud", percentage: "65%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./assets/images/skilly.png",
+    skills: [
+      { skill: "Problem Solving", percentage: "70%" },
+      { skill: "Collaboration", percentage: "80%" },
+      { skill: "Attention to details", percentage: "85%" },
+    ],
+  },
+];
+
+export const WORK_EXPERIENCE = [
+  {
+    title: "Senior Front End Developer at XLoop",
+    date: "January 2022 —— Present",
+    responsibilities: [
+      "Lead a team of frontend developer.",
+      "Implemented advance CSS animations",
+      "Collaborated with UI/UX designers",
+    ],
+  },
+  {
+    title: "Junior Web Developer at Avanza Solution",
+    date: "June 2020 —— December 2021",
+    responsibilities: [
+      "Develop and Maintain client websites",
+      "Implement SEO best practices",
+      "Work closely with design team",
+    ],
+  },
+  {
+    title: "FrontEnd Intern at Web Studio",
+    date: "May 2019 —— August 2019",
+    responsibilities: [
+      "Assist in development of e-commerce websites",
+      "Conducted performanceoptimization task",
+      "Participate in daily stand-up meetings",
+    ],
+  },
 ];
